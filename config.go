528bf5462fbbc0895cc9de2e69bfdec4ca0a905c
chenzhouyu <chenzhouyu@qq.com>
2016-03-27 00:54:52 +0800

    当为debug环境时，当没有对应的debug配置文件时，直接使用线上的配置文件

--- config.go
+++ config.go
@@ -5,6 +5,7 @@ import (
 	"git.name.im/bamboo/log.git"
 	"github.com/howeyc/fsnotify"
 	"github.com/vaughan0/go-ini"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -12,7 +13,7 @@ import (
 type conf map[string]ini.File
 
 // 对外提供的config参数
-var Config  = conf{}
+var Config = conf{}
 
 // 是否为测试环境
 var debug bool
@@ -20,17 +21,18 @@ var debug bool
 // SetDebug 设置测试环境
 func SetDebug(b bool) {
 	debug = b
+	log.SetDebug(b)
 }
+
+const (
+	NORAML_SUFFIX = ".ini"       // 正常配置文件后缀
+	DEBUG_SUFFIX  = "_debug.ini" // debug模式配置文件后缀
+)
 
 // Init 初始化配置文件
 func (c *conf) Init() {
 	// 加载所有的配置文件
-	var pattern string
-	if debug {
-		pattern = fmt.Sprintf("%s%s", c.ConfigPath(), "*_debug.ini")
-	} else {
-		pattern = fmt.Sprintf("%s%s", c.ConfigPath(), "*.ini")
-	}
+	pattern := fmt.Sprintf("%s*%s", c.ConfigPath(), NORAML_SUFFIX)
 
 	// 扫描目录找到配置文件
 	fileList, err := filepath.Glob(pattern)
@@ -40,6 +42,11 @@ func (c *conf) Init() {
 
 	// 开始加载配置文件
 	for _, v := range fileList {
+		if !debug {
+			if strings.Index(v, DEBUG_SUFFIX) > -1 {
+				continue
+			}
+		}
 		c.loadFile(v)
 	}
 
@@ -89,29 +96,51 @@ func (c *conf) Watch() {
 
 // loadFile 加载配置文件
 func (c *conf) loadFile(filePath string) bool {
-	tmp, err := ini.LoadFile(filePath)
-	if err != nil {
-		log.Warring(err)
-		return false
+	var tmp ini.File
+	var err error
+	for {
+		tmp, err = ini.LoadFile(filePath)
+		if err != nil {
+			if err == os.ErrNotExist && debug {
+				filePath = strings.Replace(filePath, DEBUG_SUFFIX, NORAML_SUFFIX, 1)
+			} else {
+				log.Warring(err)
+				return false
+			}
+		} else {
+			break
+		}
 	}
+
 	// 计算文件的名称
 	(*c)[c.fileKey(filePath)] = tmp
 	return true
 }
 
 // fileKey　通过文件地址获取文件对应的key
+// 通过 filePath 参数来获取对应的文件名做为key
 func (c *conf) fileKey(filePath string) string {
-	return strings.Replace(filepath.Base(filePath), c.fileSuffix(), "", -1)
+	return strings.Replace(filepath.Base(filePath), c.fileSuffix(filePath), "", -1)
 }
 
-// fileSuffix 配置文件后缀
-func (c *conf) fileSuffix() string {
+// fileSuffix 配置文件对应的后缀
+// filePath 当设置了文件路径的时候，通过判断是否有debug文件后缀来确定其使用的是哪种后缀
+func (c *conf) fileSuffix(filePath ...string) string {
 	var fileSuffix string
-	if debug {
-		fileSuffix = "_debug.ini"
+	if len(filePath) > 0 {
+		if strings.Index(filePath[0], DEBUG_SUFFIX) > 1 {
+			fileSuffix = DEBUG_SUFFIX
+		} else {
+			fileSuffix = NORAML_SUFFIX
+		}
 	} else {
-		fileSuffix = ".ini"
+		if debug {
+			fileSuffix = DEBUG_SUFFIX
+		} else {
+			fileSuffix = NORAML_SUFFIX
+		}
 	}
+
 	return fileSuffix
 }
 
